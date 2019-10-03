0b632b183b00290f1031eb71d636381ecffb4810
work <genjin145@gmail.com>
2019-10-03 13:43:17 +0500

    fix l

--- page/diel/js/search.js
+++ page/diel/js/search.js
@@ -32,3 +32,30 @@ function closePopupSearch() {
   popupSearch.classList.add("popup-search--hidden");
   document.body.style.overflow = "initial";
 }
+
+
+
+
+
+let searchWrapper = document.querySelector(".main-search"),
+    searchInput = searchWrapper.querySelector(".main-search__input");
+
+searchInput.addEventListener("mouseover", activateInputLine);
+searchInput.addEventListener("mouseout", deactivateInputLine);
+
+searchInput.addEventListener("focus", function() {
+  searchInput.removeEventListener("mouseout", deactivateInputLine);
+  activateInputLine();
+});
+searchInput.addEventListener("blur", function() {
+  searchInput.addEventListener("mouseout", deactivateInputLine);
+  deactivateInputLine();
+});
+
+function activateInputLine() {
+  searchInput.parentNode.classList.add("main-search--focus");
+}
+
+function deactivateInputLine() {
+  searchInput.parentNode.classList.remove("main-search--focus");
+}
