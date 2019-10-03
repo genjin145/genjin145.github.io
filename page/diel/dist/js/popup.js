018560d352ec92c6cf81e4fd334049b863f50a79
work <genjin145@gmail.com>
2019-10-03 18:36:57 +0500

    fix l4

--- page/diel/dist/js/popup.js
+++ page/diel/dist/js/popup.js
@@ -3,66 +3,85 @@ function Popup(popupClass) {
       popup,
       close;
 
-  if (document.querySelector(`.${popupClass}`)) {
-    popup = document.querySelector(`.${popupClass}`);
-    close = popup.querySelector(".popup__close");
-  } else throw(`Попап с классом .${popupClass} отсутствует!`);
+  if (document.querySelector(`${popupClass}`)) {
+    popup = document.querySelector(`${popupClass}`);
 
-  that.showPopup = function() {
-    popup.classList.add("popup--active");
-    document.body.style.overflow = "hidden";
-  }
+    if (document.querySelector(".popup__close")) {
+      close = popup.querySelector(".popup__close");
 
-  that.closePopup = function() {
-    popup.classList.remove("popup--active");
-    document.body.style.overflow = "initial";
-  }
-
-  close.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    that.closePopup();
-  });
-
-  window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode == 27) {
-      that.closePopup();
-    }
-  });
-  
-  that.addElement = function(elem) {
-    if (document.querySelector(`.${elem}`)) {
-      document.addEventListener("click", function(evt) {
-        for (let i = evt.target; i !== null; i = i.parentNode) {
-          if (i == document) break;
-          if (i.classList.contains(elem)) {
-            evt.preventDefault();
-            that.showPopup();
-            break;
-          }
-        }
-        
+      close.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        that.closePopup();
       });
-    } else throw(`Элемент с классом .${elem} отсутствует!`);
+    }
+
+    that.showPopup = function() {
+      popup.classList.add("popup--active");
+      document.body.style.overflow = "hidden";
+    }
+
+    that.closePopup = function() {
+      popup.classList.remove("popup--active");
+      document.body.style.overflow = "initial";
+    }
+
+    window.addEventListener("keydown", function(evt) {
+      if (evt.keyCode == 27) {
+        that.closePopup();
+      }
+    });
+    
+    that.addElement = function(elem) {
+      if (document.querySelector(`${elem}`)) {
+        document.addEventListener("click", function(evt) {
+          for (let i = evt.target; i !== null; i = i.parentNode) {
+            if (i == document) break;
+            if (i.classList.contains(elem)) {
+              evt.preventDefault();
+              that.showPopup();
+              break;
+            }
+          }
+          
+        });
+      }
+    }
   }
 }
 
-if (document.querySelector(".popup-leave-feedback") && document.querySelector(".popup-leave-feedback")) {
-  let feedbackPopup = new Popup("popup-leave-feedback");
+if (document.querySelector(".popup-leave-feedback") && document.querySelector(".reviews__button-primery")) {
+  let feedbackPopup = new Popup(".popup-leave-feedback");
   feedbackPopup.showPopup();
-  feedbackPopup.addElement("reviews__button-primery");
+  document.querySelector(".reviews__button-primery").addEventListener("click", function() {
+    feedbackPopup.showPopup();
+  });
 }
 
 if (document.querySelector(".popup-product-card") && document.querySelector(".product-card")) {
-  let productCardkPopup = new Popup("popup-product-card");
+  let productCardkPopup = new Popup(".popup-product-card");
   productCardkPopup.addElement("product-card");
 }
 
 if (document.querySelector(".popup-order") && document.querySelector(".card-item-form__submit")) {
-  let productCardkPopup = new Popup("popup-order");
-  productCardkPopup.addElement("card-item-form__submit");
+  let productCardkPopup = new Popup(".popup-order");
+  productCardkPopup.addElement(".card-item-form__submit");
 }
 
 if (document.querySelector(".filter") && document.querySelector(".diel-select-list__item")) {
-  let filter = new Popup("filter");
-  filter.addElement("diel-select-list__item");
+  let filter = new Popup(".filter");
+  filter.addElement(".diel-select-list__item");
+}
+
+if (document.querySelector(".popup-main-menu") && document.querySelector(".main-menu-button")) {
+  let mainMenu = new Popup(".popup-main-menu");
+  document.querySelector(".main-menu-button").addEventListener("click", function() {
+    mainMenu.showPopup();
+  });
+}
+
+if (document.querySelector(".popup-search") && document.querySelector(".user-menu__link-search")) {
+  let search = new Popup(".popup-search");
+  document.querySelector(".user-menu__link-search").addEventListener("click", function() {
+    search.showPopup();
+  });
 }
